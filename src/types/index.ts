26e34b7930b5534d1f750dfa7fed5933eb07a74d
papaponmx <jaime.rios.dev@outlook.com>
2021-06-28 08:22:35 -0500

    Add types
    Add the following types:
    * HealthData
    * UserByEmailResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,23 @@
 /**
- * Object including user metadata returned by `magic.users.getMetadataByToken`
+ * Object including user metadata returned
+ * by `magic.users.getMetadataByToken`
  */
 export type Session = {
-  createdAt: number
-  email: string
-  issuer: string
-  maxAge: number
-  publicAddress: string
-}
+  createdAt: number;
+  email: string;
+  issuer: string;
+  maxAge: number;
+  publicAddress: string;
+};
+
+export type HealthData = {
+  height: number;
+  weight: number;
+};
+
+export type UserByEmailResponse = {
+  _id: string;
+  email: string;
+  healthData: HealthData;
+  name: string;
+};
