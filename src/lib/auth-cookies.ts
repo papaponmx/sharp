f562ae41129a71a790e1d7c270c62f3bba16b069
papaponmx <jaime.rios.dev@outlook.com>
2021-06-27 23:19:26 -0500

    Refactor file to use arrow functions

--- src/lib/auth-cookies.ts
+++ src/lib/auth-cookies.ts
@@ -1,10 +1,11 @@
-import { serialize, parse } from 'cookie'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { parse, serialize } from 'cookie';
 
-const TOKEN_NAME = 'token'
+const TOKEN_NAME = 'token';
 
-export const MAX_AGE = 60 * 60 * 8 // 8 hours
+export const MAX_AGE = 60 * 60 * 8; // 8 hours
 
-export function setTokenCookie(res, token) {
+export const setTokenCookie = (res: NextApiResponse, token: string) => {
   const cookie = serialize(TOKEN_NAME, token, {
     maxAge: MAX_AGE,
     expires: new Date(Date.now() + MAX_AGE * 1000),
@@ -12,30 +13,30 @@ export function setTokenCookie(res, token) {
     secure: process.env.NODE_ENV === 'production',
     path: '/',
     sameSite: 'lax',
-  })
+  });
 
-  res.setHeader('Set-Cookie', cookie)
-}
+  res.setHeader('Set-Cookie', cookie);
+};
 
-export function removeTokenCookie(res) {
+export const removeTokenCookie = (res: NextApiResponse) => {
   const cookie = serialize(TOKEN_NAME, '', {
     maxAge: -1,
     path: '/',
-  })
+  });
 
-  res.setHeader('Set-Cookie', cookie)
-}
+  res.setHeader('Set-Cookie', cookie);
+};
 
-export function parseCookies(req) {
+export const parseCookies = (req: NextApiRequest) => {
   // For API Routes we don't need to parse the cookies.
-  if (req.cookies) return req.cookies
+  if (req.cookies) return req.cookies;
 
   // For pages we do need to parse the cookies.
-  const cookie = req.headers?.cookie
-  return parse(cookie || '')
-}
+  const cookie = req.headers?.cookie;
+  return parse(cookie || '');
+};
 
-export function getTokenCookie(req) {
-  const cookies = parseCookies(req)
-  return cookies[TOKEN_NAME]
-}
+export const getTokenCookie = (req: NextApiRequest) => {
+  const cookies = parseCookies(req);
+  return cookies[TOKEN_NAME];
+};
