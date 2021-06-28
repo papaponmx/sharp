15daf8e7747576b03087d2f42923098ea9158e2e
papaponmx <jaime.rios.dev@outlook.com>
2021-06-28 08:24:24 -0500

    Add create user mutation

--- src/graphql/queries/users.ts
+++ src/graphql/queries/users.ts
@@ -9,3 +9,15 @@ export const GET_USER_BY_EMAIL_QUERY = `query getUserByEmail($email: String!) {
     }
   }
 }`;
+
+export const CREATE_USER_MUTATION = `mutation CreateUser($userInput: UserInput!) {
+  createUser(data: $userInput) {
+    _id
+    email
+    name
+    healthData{
+      weight
+      height
+    }
+  }
+}`
