f5a4303c8482cbe44aeecdea03e15217b1edcf81
papaponmx <jaime.rios@hey.com>
2021-08-21 18:14:10 -0500

    Login page styling
    * Style login page with Tailwind.
    * Remove default styles.

--- src/lib/AuthForm.svelte
+++ src/lib/AuthForm.svelte
@@ -21,59 +21,24 @@
 	<title>Login</title>
 </svelte:head>
 
-<div class="content">
+<div
+	class="bg-gray-800 h-screen	w-full
+	flex flex-col justify-center items-center text-gray-100
+"
+>
 	<form on:submit|preventDefault={login}>
-		<label for="email">Email</label>
-		<input id="email" bind:value={email} />
+		<label class="flex flex-col gap-4 mb6" for="email"
+			>Email
+			<input
+				class="rounded text-blue-800 p-2 mb-6  focus:border-blue-600 focus:border-8 focus:bg-blue-800 focus:text-gray-100"
+				id="email"
+				bind:value={email}
+			/>
+		</label>
 		<div class="btn-container">
-			<button type="submit">Login / Signup</button>
+			<button class="rounded bg-gradient-to-r from-blue-700 to-blue-900 p-4" type="submit"
+				>Login / Signup</button
+			>
 		</div>
 	</form>
 </div>
-
-<style>
-	form {
-		width: 28rem;
-		margin: auto;
-		font-size: 1.5rem;
-	}
-
-	label {
-		display: block;
-		font-weight: bold;
-		margin-bottom: 0.25rem;
-		color: var(--text-color);
-		font-size: 16px;
-		line-height: 24px;
-	}
-
-	input {
-		background: transparent;
-		border: 1px solid #f2f0ff;
-		box-sizing: border-box;
-		height: 2.5rem;
-		margin-bottom: 0.75rem;
-		padding: 0 0.5rem;
-		width: 100%;
-		border-radius: 8px;
-	}
-
-	button {
-		background-color: var(--accent-color);
-		border-radius: 4px;
-		border: none;
-		color: white;
-		cursor: pointer;
-		font-size: 16px;
-		font-weight: bold;
-		height: 2.5rem;
-		line-height: 24px;
-		transition: background-color 0.2s ease-in-out;
-		width: 100%;
-		text-transform: uppercase;
-	}
-
-	button:hover {
-		background-color: var(--accent-hover);
-	}
-</style>
